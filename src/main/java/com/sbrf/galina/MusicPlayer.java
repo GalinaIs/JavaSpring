5cf2c777f551f4e6f4cd7c1f326575823fea0bc9
Galina Is <denis@isyanov.com>
2020-02-01 14:50:20 +0300

    Lesson 11 Qualifier

--- src/main/java/com/sbrf/galina/MusicPlayer.java
+++ src/main/java/com/sbrf/galina/MusicPlayer.java
@@ -1,27 +1,21 @@
 package com.sbrf.galina;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    @Autowired
-    private RockMusic rockMusic;
-    @Autowired
-    private ClassicalMusic classicalMusic;
-    private String name;
-    private int volume;
+    private Music music1;
+    private Music music2;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    @Autowired
+    public MusicPlayer(@Qualifier("someRockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-         return "Playing music on '" + name + "' : " + rockMusic.getSong() + " "  +
-                classicalMusic.getSong() + " in volume " + volume;
+        return "Playing music : " + music1.getSong() + ", " + music2.getSong();
     }
 }
